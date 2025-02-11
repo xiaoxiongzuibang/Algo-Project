4ee664682a4f55809c15735da2f1dcf81d39391e
xiaoxiongzuibang <167648935+xiaoxiongzuibang@users.noreply.github.com>
2025-02-11 15:48:47 +0100

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,14 @@
+from tkiteasy import *
+#from Game import Game
+
+class GUI:
+    def __init__(self, game):
+        self.game = game
+        self.root = ouvrirFenetre(500,500)
+
+
+    def correction(self):
+
+
+    def run(self):
+        self.root.mainloop()
