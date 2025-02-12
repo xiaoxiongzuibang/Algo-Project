c02ea6c12fb4f160ae3146d1928ddfef2eb623be
Yang Yinxiaotian <167648935+xiaoxiongzuibang@users.noreply.github.com>
2025-02-12 18:00:09 +0100

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -4,11 +4,15 @@ from tkiteasy import *
 class GUI:
     def __init__(self, game):
         self.game = game
-        self.root = ouvrirFenetre(500,500)
+        self.root = ouvrirFenetre(2000,2000)
+        self.matrix = game.generate_board()
 
-
-    def correction(self):
-
+    def initialization(self):
+        for i in range(6):
+            for j in range(6):
+                self.root.dessinerRectangle(i*200, j*200, 500, 500, "orange")
+                self.root.afficherTexte(self.matrix[i][j], i*200+100, j*200+100, col="black", sizefont=70)
 
     def run(self):
+        self.initialization()
         self.root.mainloop()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Game import Game
 from GUI import GUI
+from CreatSet import *
 
 if __name__ == '__main__':
-    game = Game()
+    game = Game(french_words)
     gui = GUI(game)
     gui.run()
