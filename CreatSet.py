1622a50aae9c264f405e293b49ffa7b8f8eb6970
ryan-zoz <ryanzou000@gmail.com>
2025-02-14 23:44:01 +0100

    Update CreatSet.py

--- CreatSet.py
+++ CreatSet.py
@@ -1,9 +1,13 @@
 
 import pandas as pd
 
-# 读取 TSV 文件
-df = pd.read_csv("Lexique383.tsv", delimiter="\t", encoding="utf-8")
+# read csv file
+df = pd.read_csv("dictionnaire_fr.csv",header=None )
 
-# 提取单词列并转换为 set
-french_words = set(df["ortho"])
+# transforme csv file to set
+french_words = set(df[0])
+
+# test complete
+
+
 
